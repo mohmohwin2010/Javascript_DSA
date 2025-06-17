13f0fb5ac12797322d900213f04713fa3443fea8
mohmohwin2010 <mohmohwin2010@gmail.com>
2025-06-17 18:01:52 +0630

    added code

--- Question 1/index.js
+++ Question 1/index.js
@@ -2,3 +2,13 @@
 Question 1 : sum of natural numbers from 1 to n.
 eg. sum of 1 to 5: 1+2+3+4+5=15
 */
+
+function sumNum(num){
+    let sum = 0;
+    for(let i=1; i<=num; i++){
+        sum = sum + i;
+    }
+    return sum;
+}
+console.log(sumNum(5));
+console.log(sumNum(10));
