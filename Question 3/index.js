422c235256bdb3221ef5c8e54a0bfa8ed6ce2076
mohmohwin2010 <mohmohwin2010@gmail.com>
2025-06-19 07:00:33 +0630

    added question 3

--- Question 3/index.js
+++ Question 3/index.js
@@ -1,4 +1,47 @@
 /*
 Question 3 : count the number of digit of a number
-12:31
+input : 47836
+output : 5
+explaination
+===========
+47836
+4783
+47
+4
+0
+
+input : -87342
+output : 5
+
+Math.abs() remove negative sign.
+
 */
+
+// function countDigit(num){
+//     let num1 = Math.abs(num);
+//     let count = 0;
+    
+//     // do{
+//     //     count++;
+//     //     num1 =  Math.floor(num1/10);
+//     // }while(num1 > 0);
+//     while(num1 > 0){
+//         count = count + 1;
+//         num1 = Math.floor(num1/10)
+//     }
+//     return count;
+// }
+// console.log(countDigit(478036))
+// console.log(countDigit(-89234532))
+
+// my practise
+function countDigitNum(num){
+    let numabs = Math.abs(num);
+    let count = 0;
+    while(numabs > 0){
+        count = count + 1;
+        numabs = Math.floor(numabs/10);
+    }
+    return count;
+}
+console.log(countDigitNum(432983));
