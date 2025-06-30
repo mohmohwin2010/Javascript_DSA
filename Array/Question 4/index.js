af3125e11de615926ab58652b96d21f4eca7d9f8
mohmohwin2010 <mohmohwin2010@gmail.com>
2025-06-30 07:55:29 +0630

    added array manipulation

--- Array/Question 4/index.js
+++ Array/Question 4/index.js
@@ -6,3 +6,31 @@ console.log(arrySort);
 //reverse sort
 arrySort.sort((a, b) => b - a);
 console.log(arrySort)
+
+//How to reverse array?
+arrySort.reverse()
+console.log(arrySort)
+
+//Array manipulation : Map, Filter, Reduce
+const newMapArr = arrySort.map((elem, i) => elem*elem);
+console.log(newMapArr);
+
+const newFilterArr = arrySort.filter((elem, i) => elem > 0);
+console.log(newFilterArr);
+
+const newReduceArr =  newFilterArr.reduce((acc, elem) => acc+elem, 0);
+console.log(newReduceArr)
+
+//Flat
+
+const deep = [1, [2, [3, 8, [4]]]];
+// console.log(deep)
+// console.log(deep.flat(1));
+// console.log(deep.flat(2));
+// console.log(deep.flat(3))
+console.log(deep.flat(Infinity)); // [1, 2, 3, 4]
+
+//flat(1), it flattens only one level.
+
+//flat(Infinity), it flattens everything, no matter how deep.
+
